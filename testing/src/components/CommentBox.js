9e12a3269a33b5544e02374a9f70b1f6eebde93e
Robert Checco <robert.checco@gmail.com>
2019-07-07 18:44:16 -0600

    Render the comment box and the comment list to client via app component

---
+++ testing/src/components/CommentBox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CommentBox = () => {
+    return (
+        <div>This is the comment box</div>
+    );
+}
+
+export default CommentBox;
---
+++ testing/src/components/CommentList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CommentList = () => {
+    return (
+        <div>This is the comment list</div>
+    );
+}
+
+export default CommentList;
