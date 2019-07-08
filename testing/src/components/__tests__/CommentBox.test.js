b4b4bfab5716c1771d7cf0d4a88fda7c4658e80d
Robert Checco <robert.checco@gmail.com>
2019-07-08 12:15:56 -0600

    Pre expectation: it has a text area a user can type in

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -18,3 +18,10 @@ it('has a text area and a button', () => {
     expect(wrapped.find('button').length).toEqual(1);
 
 });
+
+it('has a text area that users can type in', () => {
+    wrapped.find('textarea').simulate('change', {
+        target: { value: 'new comment' }
+    });
+    wrapped.update();
+});
