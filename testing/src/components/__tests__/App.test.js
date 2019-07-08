74c4208c15f9d5be2c404f115640270f393e267f
Robert Checco <robert.checco@gmail.com>
2019-07-07 19:24:06 -0600

    Typo of DOM Manipulation: Quick Fix

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -1,5 +1,19 @@
 /*
 Since it is in the __test__ we did not need it to be
+
+The It Function: it is a global function, we do not need to import it into out test file
+
+Organize the tests in the application.
+
+Arguments to the Function
+1. The string that is going to represent what we are going to do...
+2. ??? The tests that we are going to run. ???
+
+Key Points:
+This is not running in a browser! This is being ran in a virtual environment
+that exists in the command line, and not in any browser! No Chrome or Firefox or anything like that.
+
+
 */
 
 import React from 'react';
@@ -7,3 +21,19 @@ import ReactDOM from 'react-dom';
 
 import App from '../App';
 
+it('shows a box', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(App, div);
+
+    // Now we can write code that will inspect that div element!
+    // and checks that the comment box is in there!
+
+
+    // What in the world is this line doing?
+    // CLEANUP!   
+    // It is going to remove the App Component from the root div!
+
+
+    ReactDOM.unmountComponentAtNode(div);
+});
