7f5622b33350fa200087ff3c3e9dcfea57f5c95f
Robert Checco <robert.checco@gmail.com>
2019-07-19 18:45:48 -0400

    Testing the comment list

--- testing/src/components/__tests__/CommentList.test.js
+++ testing/src/components/__tests__/CommentList.test.js
@@ -21,3 +21,20 @@ beforeEach(() => {
 it('creates one LI per comment', () => {
     expect(wrapped.find('li').length).toEqual(2);
 });
+
+it('shows the text for each comment', () => {
+    expect(wrapped.render().text()).toContain('Comment 1');
+    expect(wrapped.render().text()).toContain('Comment 2');
+});
+
+// Two different ways to examine the html? 
+/*
+.text => String : Returns a string of the rendered text of the current render tree. Enzyme says should be used with skepticism...
+
+Enzyme Recommendation:
+.render() => CheerioWrapper
+
+Returns a CheerioWrapper around the rendered HYML of the current node's subtree
+
+What is Cheerio.js? It is a Web Scraping Library that uses jquery syntax.
+*/
