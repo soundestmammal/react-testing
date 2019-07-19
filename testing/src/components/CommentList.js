c7f7a52c19f939d8462f9a9dbf2279dce1a8625a
Robert Checco <robert.checco@gmail.com>
2019-07-19 18:37:54 -0400

    Refactor to a class based component

--- testing/src/components/CommentList.js
+++ testing/src/components/CommentList.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const CommentList = () => {
-    return (
-        <div>This is the comment list</div>
-    );
+
+class CommentList extends Component {
+    renderComments() {
+        return this.props.comments.map(comment => {
+            return <li key={comment}>{comment}</li>
+        })
+    }
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.renderComments()}
+                </ul>
+            </div>
+        );
+    }
 }
 
-export default CommentList;
+const mapStateToProps = state => {
+    return { comments: state.comments };
+}
+
+export default connect(mapStateToProps)(CommentList);
