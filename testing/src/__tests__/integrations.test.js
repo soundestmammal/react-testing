899f1b32a18503c170536f56eda84af425c8d8a9
Robert Checco <robert.checco@gmail.com>
2019-07-20 13:35:39 -0400

    Start on writing a full integration test for application

---
+++ testing/src/__tests__/integrations.test.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import moxios from 'moxios';
+import Root from 'root';
+import App from 'components/App';
+
+beforeEach(() => {
+    moxios.install();
+    moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+        status: 200,
+        response: [{ name: 'Fetched #1' }, { name: 'Fetched #2'}]
+    });
+});
+
+afterEach(() => {
+    moxios.uninstall();
+});
+
+it('can fetch a list of comments and display them', () => {
+    // Attempt to render the entire app
+        const wrapped = mount(
+            <Root>
+                <App />
+            </Root>
+        );
+    
+    // find the 'fetch comments' button and click it
+    wrapped.find('.fetch-comments').simulate('click');
+    
+
+    // Expect to find a list of comments!
+    expect(wrapped.find('li').length).toEqual(2);
+});
+
+/*
+Simulate click event on the button element
+
+Action creator gets called
+
+Request is sent
+
+Test fails
+
+We are going to trick axios into thinking that it is working...
+
+Moxios is going to return data from the api and fake out axios to think
+that a request was successfully issued...
+*/
