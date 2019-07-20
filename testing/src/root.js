325f5ee0ad165928f25a0935aba49c6a7de24dc7
Robert Checco <robert.checco@gmail.com>
2019-07-20 13:40:15 -0400

    Work around for testing with redux and async requests

--- testing/src/root.js
+++ testing/src/root.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from 'reducers';
+import reduxPromise from 'redux-promise';
+
 
 export default ({ children, initialState = {} }) => {
+
+    const store = createStore(
+        reducers,
+        initialState,
+        applyMiddleware(reduxPromise)
+    );
+
     return (
-    <Provider store={createStore(reducers, initialState)}>
+    <Provider store={store}>
         {children}
     </Provider>
     );
