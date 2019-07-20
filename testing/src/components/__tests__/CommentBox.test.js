393ce5794cb6b79841282deb7c2ccbc6a05a46ef
Robert Checco <robert.checco@gmail.com>
2019-07-20 13:39:44 -0400

    Now there are two buttons (submit&fetch), change test to equal 2

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -15,7 +15,7 @@ afterEach(() => {
 it('has a text area and a button', () => {
     // expect(wrapped.find(<button/>, <textarea/>));
     expect(wrapped.find('textarea').length).toEqual(1);
-    expect(wrapped.find('button').length).toEqual(1);
+    expect(wrapped.find('button').length).toEqual(2);
 
 });
 
