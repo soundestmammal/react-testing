61c85cb3afb8aca05413886158a01cd40bb731b7
Robert Checco <robert.checco@gmail.com>
2019-07-22 13:29:36 -0400

    Refactor to a class based component and add conditional logic

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -1,14 +1,71 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
 
 
-export default () => {
-    return(
-        <div>
-            <Route path='/' exact component={CommentList} />
-            <Route path='/post' component={CommentBox} />
-        </div>
-    );
+class App extends Component {
+
+    renderButton() {
+        if(this.props.auth) {
+            return (
+                <button onClick={() => this.props.changeAuth(false)}>
+                    Sign Out
+                </button>
+            );
+        } else {
+            return (
+                <button onClick={() => this.props.changeAuth(true)}>
+                    Sign In
+                </button>
+            );
+        }
+    }
+
+    renderHeader() {
+        return(
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/post'>Post a comment</Link>
+                </li>
+                <li>
+                    {this.renderButton()}
+                    <Link></Link>
+                </li>                
+            </ul>
+        )
+    }
+    render() {
+        return(
+            <div>
+                {this.renderHeader()}
+                <Route path='/' exact component={CommentList} />
+                <Route path='/post' component={CommentBox} />
+            </div>
+        );
+    }   
 }
+
+const mapStateToProps = (state) => {
+    return { auth: state.auth };
+}
+
+export default connect(mapStateToProps, actions)(App);
+
+
+
+/* Change to a class based component to have access to a few helper methods:
+
+Which ones?
+
+renderHeader
+1. Home
+2. Post
+3. Sign in
+
+*/
