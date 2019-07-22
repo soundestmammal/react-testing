0837f8ef84227fd358bcb2da0a3c0f5a8a30a7f2
Robert Checco <robert.checco@gmail.com>
2019-07-22 12:54:47 -0400

    Import CHANGE_AUTH type and add changeAuth action creator

--- testing/src/actions/index.js
+++ testing/src/actions/index.js
@@ -1,4 +1,4 @@
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 import axios from 'axios';
 
 export const saveComment = (comment) => {
@@ -16,4 +16,11 @@ export const fetchComments = () => {
         type: FETCH_COMMENTS,
         payload: response
     }
+}
+
+export const changeAuth = (isLoggedIn) => {
+    return ({
+        type: CHANGE_AUTH,
+        payload: isLoggedIn
+    });
 }
