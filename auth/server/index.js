09509c2614a1821649d59c3d33cd19fea9ea7d14
Robert Checco <robert.checco@gmail.com>
2019-07-25 19:50:56 -0400

    Initial server set up and use morgan and bodyparser middlewares

---
+++ auth/server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// Application Setup
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*'}));
+
+// Server Setup
+const PORT = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(PORT);
+console.log("Server listening on Port: ", PORT);
+ 
+/*
+app.use is to use middlewares,
+morgan is a logging framework! logging incoming requests
+
+bodyParser: will parse them into json that is incoming
+*/
