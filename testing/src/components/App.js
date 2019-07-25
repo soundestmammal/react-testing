bba21596c148f7f56cf5621b967b2e1df59eb916
Robert Checco <robert.checco@gmail.com>
2019-07-25 13:19:54 -0400

    Quick fix, remove because it was throwing an error

--- testing/src/components/App.js
+++ testing/src/components/App.js
@@ -35,7 +35,6 @@ class App extends Component {
                 </li>
                 <li>
                     {this.renderButton()}
-                    <Link></Link>
                 </li>                
             </ul>
         )
