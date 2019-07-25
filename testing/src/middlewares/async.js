e5873c52d5774f56af938db21ffd9cd9b11a9d4a
Robert Checco <robert.checco@gmail.com>
2019-07-25 13:20:33 -0400

    Make middleware to see how it works behind the scenes in the future

---
+++ testing/src/middlewares/async.js
@@ -0,0 +1,28 @@
+/* export default function({ dispatch }) {
+    return function (next) { // next is a function
+        return function(action) {
+            return action;
+        }
+    }
+}
+*/
+
+export default ({dispatch}) => next => action => {
+    // Check to see if the action
+    // has a promise on payload property
+    // If it does, then wait to resolve
+    // If it doesn't, send to the next middleware
+    if(!action.payload || !action.payload.then) {
+        return next(action);
+    }
+    // We want to wai for the promise to resolve
+    // (get its data) and then create a new action
+    // with that data and dispatch it
+
+    action.payload.then(function (response) {
+        const newAction = { ...action, payload: response };
+        dispatch(newAction);
+    });
+}
+    
+        
