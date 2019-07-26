46f207ff6ce000de9da957efa72bccca9f9c3b34
Robert Checco <robert.checco@gmail.com>
2019-07-25 20:00:31 -0400

    Import routes file into index.js, wrap the express server with routes

--- auth/server/index.js
+++ auth/server/index.js
@@ -2,18 +2,20 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const router = require('./router');
 
 // Application Setup
 const app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*'}));
+router(app);
 
 // Server Setup
 const PORT = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(PORT);
 console.log("Server listening on Port: ", PORT);
- 
+
 /*
 app.use is to use middlewares,
 morgan is a logging framework! logging incoming requests
