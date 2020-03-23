6571b0eaf27a1c17a8801517f927805f8f554c0c
Rob <robert.checco@gmail.com>
2020-03-22 20:25:37 -0400

    Signup logic and use in the router

--- auth/server/router.js
+++ auth/server/router.js
@@ -1,5 +1,5 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = function(app) {
-    app.get('/', (req, res, next) => {
-        res.send(['ipad', 'iphone', 'macbook air', 'waterbottle']);
-    });
+    app.post('/signup', Authentication.signup);
 }
