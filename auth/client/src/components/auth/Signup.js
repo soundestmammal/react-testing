f231a107d50de36cad177ffc9005792ac9635cb8
Rob <robert.checco@gmail.com>
2020-03-24 18:59:35 -0400

    Add redux form to project for sign up

--- auth/client/src/components/auth/Signup.js
+++ auth/client/src/components/auth/Signup.js
@@ -1,18 +1,40 @@
 import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+
 
 class Signup extends Component {
+
+
+    onSubmit = (formProps) => {
+        console.log(formProps);
+    };
+
     render() {
+        const { handleSubmit } = this.props;
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit)}>
                 <fieldset>
                     <label>Email</label>
+                    <Field
+                        name="email"
+                        type="text"
+                        component="input"
+                        autoComplete="none"
+                    />
                 </fieldset>
                 <fieldset>
                     <label>Password</label>
+                    <Field
+                    name="password"
+                    type="password"
+                    component="input"
+                    autoComplete="none"
+                    />
                 </fieldset>
+                <button>Signup</button>
             </form>
         )
     }
 }
 
-export default Signup;
+export default reduxForm({ form: 'signup' })(Signup);
