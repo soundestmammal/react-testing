64c6bd9cc554910665cdc793e5f334922a508559
Rob <robert.checco@gmail.com>
2020-03-31 18:18:10 -0400

    Signup AC - dispatches actions based on signup response

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -1,9 +1,19 @@
 import axios from 'axios';
-import { AUTH_USER } from './types';
+import { AUTH_USER, AUTH_ERROR } from './types';
 
 // REDUX THUNK returns a function called dispatch
-export const signup = (formProps) => dispatch => {
-    axios.post('http://localhost:3090/signup', formProps);
+export const signup = (formProps, callback) => async dispatch => {
+    try {
+        const response = await axios.post('http://localhost:3090/signup', formProps);
+
+        dispatch({ type: AUTH_USER, payload: response.data.token });
+        callback();
+    } catch (e) {
+        dispatch({ type: AUTH_ERROR, payload: 'Email in use' })
+    }
+    
+
+    // We will return back to this action creator soon in the future.
 }
 
 /* Redux Thunk, we can dispatch as many actions as we choose,
