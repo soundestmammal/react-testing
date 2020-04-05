70aa8a28e9d0a6af695c7b18ab91b3874fe86830
Rob <robert.checco@gmail.com>
2020-04-05 18:43:01 -0400

    Signin and signout Action Creators

--- auth/client/src/actions/index.js
+++ auth/client/src/actions/index.js
@@ -1,21 +1,31 @@
 import axios from 'axios';
 import { AUTH_USER, AUTH_ERROR } from './types';
 
-// REDUX THUNK returns a function called dispatch
 export const signup = (formProps, callback) => async dispatch => {
     try {
         const response = await axios.post('http://localhost:3090/signup', formProps);
 
         dispatch({ type: AUTH_USER, payload: response.data.token });
+        localStorage.setItem('token', response.data.token);
         callback();
     } catch (e) {
         dispatch({ type: AUTH_ERROR, payload: 'Email in use' })
     }
-    
-
-    // We will return back to this action creator soon in the future.
 }
 
-/* Redux Thunk, we can dispatch as many actions as we choose,
-for Redux Promise, we can dispatch only one action?
-*/
+export const signout = () => {
+    localStorage.removeItem('token');
+    return { type: AUTH_USER, payload: '' }
+}
+
+export const signin = (formProps, callback) => async dispatch => {
+    try {
+        const response = await axios.post('http://localhost:3090/signin', formProps);
+
+        dispatch({ type: AUTH_USER, payload: response.data.token });
+        localStorage.setItem('token', response.data.token);
+        callback();
+    } catch (e) {
+        dispatch({ type: AUTH_ERROR, payload: 'Invalid Credentials' });
+    }
+}
