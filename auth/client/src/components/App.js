64a19ace0bd3bdeabeafb34f0e069327c0cf0043
Rob <robert.checco@gmail.com>
2020-04-09 14:54:18 -0400

    Add Landing Component to App

--- auth/client/src/components/App.js
+++ auth/client/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Landing from './Landing';
 
 export default ({ children }) => {
     return(
         <div>
             <Header />
+            <Landing />
             { children }
         </div>
     );
