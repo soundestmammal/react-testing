62a2530e373da660abfaaeb09c4486861eff6cc3
Rob <robert.checco@gmail.com>
2020-04-09 14:51:50 -0400

    Add image to landing page

---
+++ auth/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './LandingStyle.css';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div>
+                <div className="landing-image"> This is the landing page!!!</div>
+            </div>
+        )
+    }
+}
+
+export default Landing;
