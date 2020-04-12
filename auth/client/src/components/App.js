d05f914314446f88cdab9ea12c9eb6264b56fdcc
Rob <robert.checco@gmail.com>
2020-04-12 15:09:28 -0400

    No longer show the Landing component out of the app comp.

--- auth/client/src/components/App.js
+++ auth/client/src/components/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from './Header';
-import Landing from './Landing';
 
 export default ({ children }) => {
     return(
         <div>
             <Header />
-            <Landing />
             { children }
         </div>
     );
