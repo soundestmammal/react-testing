01cd4fba8c0e227337d8cebe23a11260ada68506
Rob <robert.checco@gmail.com>
2020-04-12 15:08:39 -0400

    Add new routes to project
    Landing, dashboard, profile

--- auth/client/src/index.js
+++ auth/client/src/index.js
@@ -7,11 +7,13 @@ import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 import App from './components/App';
-import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 import Feature from './components/Feature';
 import Signout from './components/auth/Signout';
 import Signin from './components/auth/Signin';
+import Landing from './components/Landing';
+import Dashboard from './components/Dashboard';
+import Profile from './components/Profile';
 
 const store = createStore(
     reducers,
@@ -25,11 +27,13 @@ ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
             <App> 
-                <Route path="/" exact component={Welcome} />
+                <Route path="/" exact component={Landing} />
                 <Route path="/signup" component={Signup} />
                 <Route path="/feature" component={Feature} />
                 <Route path="/signout" component={Signout} />
                 <Route path="/signin" component={Signin} />
+                <Route path="/dashboard" component={Dashboard} />
+                <Route path="/profile" component={Profile} />
             </App>
         </BrowserRouter>
     </Provider>, 
